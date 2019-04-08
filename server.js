abfa9b25454221ed1a89347144cb2bf59960172d
atiftariq786 <atiftariq786@hotmail.com>
2019-04-07 22:06:15 -0700

    add startup file, install npm pakges configuration and server.js code added

---
+++ server.js
@@ -0,0 +1,8 @@
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("bosy-parser");
+var express = require("express");
+var express = require("express");
+var express = require("express");
