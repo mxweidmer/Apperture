bd165dff57456b83b81ab11237536e7e293cf169
ctghadiri <ctghadiri@gmail.com>
2019-04-10 18:31:18 -0700

    add js file for html events

---
+++ app/public/assets/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("signin-btn").on("click", function (event){
+        event.preventDefault();
+
+    })
+    $("signup-btn").on("click", function (event){
+        event.preventDefault();
+        
+    })
+    $("")
+})
